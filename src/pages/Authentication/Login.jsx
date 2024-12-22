f73ec13895b25ab104f587112f3687c8f389b81f
RaiyanJiyon <raiyanjiyon814@gmail.com>
2024-12-22 11:22:59 +0600

    Add user authentication components: Register and Login
    - Implemented Register component with form validation for name, photo URL, email, and password
    - Added password strength validation and matching confirmation password
    - Included Google sign-up functionality in Register component with success and error toasters
    - Implemented Login component with form validation for email and password
    - Included Google login functionality in Login component with success and error toasters
    - Both components redirect users to the home page upon successful authentication

--- src/pages/Authentication/Login.jsx
+++ src/pages/Authentication/Login.jsx
@@ -1,8 +1,113 @@
+import { useContext, useEffect } from "react";
+import { Link, useLocation, useNavigate } from "react-router-dom";
+import { authContext } from "../../contexts/AuthProvider";
+import SuccessToaster from "../../components/ToasterNotification/SuccessToaster";
+import ErrorToaster from "../../components/ToasterNotification/ErrorToaster";
+import { FaGithub, FaGoogle } from "react-icons/fa";
+
+
 const Login = () => {
-    return (
-        <div>
+    useEffect(() => {
+        window.scrollTo(0, 0);
+    }, []);
+
+    const location = useLocation();
+    const navigate = useNavigate();
+
+    const { createUserWithGoogle, loginUser } = useContext(authContext);
+
+    const handleLoginWithGoogle = () => {
+        createUserWithGoogle()
+            .then(result => {
+                console.log(result.user);
+                SuccessToaster("Successfully Logged In with Google");
+                navigate(location?.state ? location.state : "/");
+            })
+            .catch(error => {
+                console.error(error.message);
+                ErrorToaster(error.message);
+            });
+        };
+        
+        const handleLoginForm = (e) => {
+            e.preventDefault();
             
-        </div>
+        const form = e.currentTarget;
+        const formData = new FormData(form);
+
+        const email = formData.get("email");
+        const password = formData.get("password");
+        
+        console.log(email, password);
+        
+        loginUser(email, password)
+        .then(userCredential => {
+            console.log(userCredential.user);
+            SuccessToaster("Successfully Logged In");
+            navigate(location?.state ? location.state : '/');
+        })
+        .catch(error => {
+            console.error(error.message);
+            ErrorToaster(error.message);
+            });
+    };
+
+    const navigateToForgetPassword = () => {
+        const emailInput = document.getElementById('email');
+        navigate("/auth/forgot-password", {
+            state: {
+                email: emailInput.value,
+            }
+        });
+    }
+
+    return (
+        <section className="bg-gray-50 mt-6">
+            <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto lg:py-0">
+                <div className="w-full bg-white rounded-lg shadow border md:mt-0 sm:max-w-md xl:p-0">
+                    <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
+                        <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl">
+                            Sign in to your account
+                        </h1>
+                        <form onSubmit={handleLoginForm} className="space-y-4 md:space-y-6" action="#">
+                            <div className="flex flex-col md:flex-row items-center justify-between gap-2">
+                                <div onClick={handleLoginWithGoogle} className="flex items-center md:justify-center gap-2 w-full border border-gray-300 px-4 py-2 rounded-lg cursor-pointer">
+                                    <FaGoogle className="text-xl" />
+                                    <span className="text-sm font-medium">Log in with Google</span>
+                                </div>
+                                <div className="flex items-center md:justify-center gap-2 w-full border border-gray-300 px-4 py-2 rounded-lg cursor-pointer">
+                                    <FaGithub className="text-xl" />
+                                    <span className="text-sm font-medium">Log in with Github</span>
+                                </div>
+                            </div>
+                            <div>
+                                <label htmlFor="email" className="block mb-2 text-sm font-medium text-gray-900">Your email</label>
+                                <input type="email" name="email" id="email" className="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5" placeholder="name@company.com" required />
+                            </div>
+                            <div>
+                                <label htmlFor="password" className="block mb-2 text-sm font-medium text-gray-900">Password</label>
+                                <input type="password" name="password" id="password" placeholder="••••••••" className="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5" required />
+                            </div>
+                            <div className="flex items-center justify-between">
+                                <div className="flex items-start">
+                                    <div className="flex items-center h-5">
+                                        <input id="remember" aria-describedby="remember" type="checkbox" className="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-primary-300" />
+                                    </div>
+                                    <div className="ml-3 text-sm">
+                                        <label htmlFor="remember" className="text-gray-500">Remember me</label>
+                                    </div>
+                                </div>
+                                <span onClick={navigateToForgetPassword} className="text-sm font-medium text-black hover:underline">Forgot password?</span>
+                            </div>
+                            <button type="submit" className="w-full bg-black hover:bg-[#c59d5f] text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-bold rounded-lg text-sm px-5 py-2.5 text-center">Sign in</button>
+                            <p className="text-sm font-light text-gray-500">
+                                Don’t have an account yet? <Link to={"/register"} className="font-medium text-black hover:underline">Sign up</Link>
+                            </p>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </section>
     );
 };
 
--- src/pages/Authentication/Register.jsx
+++ src/pages/Authentication/Register.jsx
@@ -1,8 +1,133 @@
+import { useContext, useEffect } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { authContext } from "../../contexts/AuthProvider";
+import SuccessToaster from "../../components/ToasterNotification/SuccessToaster";
+import ErrorToaster from "../../components/ToasterNotification/ErrorToaster";
+import { FaGithub, FaGoogle } from "react-icons/fa";
+
+
 const Register = () => {
+    useEffect(() => {
+        window.scrollTo(0, 0);
+    }, []);
+
+    const navigate = useNavigate();
+
+    const { user, setUser, createUserWithGoogle, createUser } = useContext(authContext);
+    console.log(user);
+
+    const handleGoogleSignUP = () => {
+        createUserWithGoogle()
+            .then(result => {
+                console.log(result.user);
+                SuccessToaster("Successfully Sign In with Google");
+                navigate("/");
+            })
+            .catch(error => {
+                console.error("Error during Google sign-up:", error.message);
+                ErrorToaster(error.message);
+            });
+    };
+
+    const handleRegisterForm = (e) => {
+        e.preventDefault();
+
+        const form = e.currentTarget;
+        const formData = new FormData(form);
+
+        const name = formData.get('name');
+        const photoURL = formData.get('photoURL');
+        const email = formData.get('email');
+        const password = formData.get('password');
+        const confirmPassword = formData.get('confirm-password');
+
+        console.log(name, photoURL, email, password, confirmPassword);
+
+        const validPassword = /^(?=.*[a-z])(?=.*[A-Z]).{6,}$/;
+
+        if (!validPassword.test(password)) {
+            ErrorToaster("Password should be at least 8 character");
+            return;
+        };
+        
+        if (password !== confirmPassword) {
+            ErrorToaster("The password confirmation does not match.");
+            return;
+        }
+
+        createUser(email, password)
+            .then(userCredential => {
+                console.log(userCredential.user);
+                SuccessToaster("Successfully Signed In")
+                setUser({user, 
+                    displayName: name,
+                    photoURL: photoURL
+                })
+                navigate("/");
+                form.reset();
+            })
+            .catch(error => {
+                console.log(error.message);
+                ErrorToaster(error.message);
+            });
+    };
+
     return (
-        <div>
-            
-        </div>
+        <section className="bg-gray-50">
+            <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto mt-6 lg:py-0">
+                <div className="w-full bg-white rounded-lg shadow border md:mt-0 sm:max-w-md xl:p-0">
+                    <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
+                        <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl">
+                            Create an account
+                        </h1>
+                        <form onSubmit={handleRegisterForm} className="space-y-4 md:space-y-6" action="#">
+                            <div className="flex flex-col md:flex-row items-center justify-between gap-2">
+                                <div onClick={handleGoogleSignUP} className="flex items-center md:justify-center gap-2 w-full border border-gray-300 px-4 py-2 rounded-lg cursor-pointer">
+                                    <FaGoogle className="text-xl" />
+                                    <span className="text-xs font-medium">Sign up with Google</span>
+                                </div>
+                                <div className="flex items-center md:justify-center gap-2 w-full border border-gray-300 px-4 py-2 rounded-lg cursor-pointer">
+                                    <FaGithub className="text-xl" />
+                                    <span className="text-xs font-medium">Sign up with Github</span>
+                                </div>
+                            </div>
+                            <div>
+                                <label htmlFor="name" className="block mb-2 text-sm font-medium text-gray-900">Your name</label>
+                                <input type="text" name="name" id="name" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5" placeholder="name" required />
+                            </div>
+                            <div>
+                                <label htmlFor="photoURL" className="block mb-2 text-sm font-medium text-gray-900">Your photo url</label>
+                                <input type="text" name="photoURL" id="photoURL" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5" placeholder="photo url" required />
+                            </div>
+                            <div>
+                                <label htmlFor="email" className="block mb-2 text-sm font-medium text-gray-900">Your email</label>
+                                <input type="email" name="email" id="email" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5" placeholder="name@company.com" required />
+                            </div>
+                            <div>
+                                <label htmlFor="password" className="block mb-2 text-sm font-medium text-gray-900">Password</label>
+                                <input type="password" name="password" id="password" placeholder="••••••••" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5" required />
+                            </div>
+                            <div>
+                                <label htmlFor="confirm-password" className="block mb-2 text-sm font-medium text-gray-900">Confirm password</label>
+                                <input type="password" name="confirm-password" id="confirm-password" placeholder="••••••••" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5" required />
+                            </div>
+                            <div className="flex items-start">
+                                <div className="flex items-center h-5">
+                                    <input id="terms" aria-describedby="terms" type="checkbox" className="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-primary-300" required />
+                                </div>
+                                <div className="ml-3 text-sm">
+                                    <label htmlFor="terms" className="font-light text-gray-500">I accept the <a className="font-medium text-primary-600 hover:underline" href="#">Terms and Conditions</a></label>
+                                </div>
+                            </div>
+                            <button type="submit" className="w-full bg-black hover:bg-[#c59d5f] text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-bold rounded-lg text-sm px-5 py-2.5 text-center">Create an account</button>
+                            <p className="text-sm font-light text-gray-500">
+                                Already have an account? <Link to={"/login"} className="font-medium text-black hover:underline">Login here</Link>
+                            </p>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </section>
     );
 };
 
