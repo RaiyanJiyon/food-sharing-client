74856fdbe85ef2d6ada7679af31cf4bdd9f22afa
RaiyanJiyon <raiyanjiyon814@gmail.com>
2024-12-22 12:47:48 +0600

    Add ErrorPage component to handle 404 errors
    - Implemented ErrorPage component with a custom 404 image
    - Displayed an informative message for users who encounter a 404 error
    - Included a 'Go Back Home' button to redirect users to the homepage

--- src/pages/ErrorPage.jsx
+++ src/pages/ErrorPage.jsx
@@ -1,7 +1,18 @@
+import { Link } from "react-router-dom";
+import ErrorImg from '../assets/images/404.webp'
+
 const ErrorPage = () => {
     return (
-        <div>
-            
+        <div className='h-screen md:w-1/2 mx-auto flex flex-col justify-center items-center'>
+            <div className="w-2/5 mx-auto">
+                <img className="" src={ErrorImg} alt="404 image" />
+            </div>
+            <h1 className='text-3xl font-bold text-center'>404 : Page Not Found</h1>
+            <p className='text-xl font-medium text-center mt-4'>We looked everywhere for this page. Are you sure the website URL is correct? Get in touch with the site owner.</p>
+
+            <Link to={"/"} className="mt-4">
+                <button className='btn bg-[#c59d5f] text-white font-bold rounded-[32px]'>Go Back Home</button>
+            </Link>
         </div>
     );
 };
